a949d87caa0af71d8426a929eee2055849c710a6
MartinNorberg <martin.norberg@gautomation.se>
2020-10-20 17:36:18 +0200

    Added ViewModel and dataContext to MainWindow

---
+++ PhoenixTelemetryTransfer/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PhoenixTelemetryTransfer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
