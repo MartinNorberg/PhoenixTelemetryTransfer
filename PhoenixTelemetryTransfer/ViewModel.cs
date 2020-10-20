6b8cb77d8eacc0539b7c06b803f6c414e92d8b8f
MartinNorberg <martin.norberg@gautomation.se>
2020-10-20 21:15:53 +0200

    Added some temporary graphic....

--- PhoenixTelemetryTransfer/ViewModel.cs
+++ PhoenixTelemetryTransfer/ViewModel.cs
@@ -7,14 +7,36 @@
     using System.Runtime.CompilerServices;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Windows.Input;
 
     public class ViewModel: INotifyPropertyChanged
     {
+        private string path;
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ViewModel()
         {
         }
+
+        public string Path
+        {
+            get => this.path;
+            set
+            {
+                if (Equals(this.path, value))
+                {
+                    return;
+                }
+
+                this.path = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ICommand BrowseCommand { get; }
+
+        public ICommand StartCommand { get; }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
