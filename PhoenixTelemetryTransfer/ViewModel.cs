4162e55c5552594d33aa3f6c8f5e70d064a11d97
MartinNorberg <martin.norberg@gautomation.se>
2020-10-26 18:23:37 +0100

    Making stylecop happy :)

--- PhoenixTelemetryTransfer/ViewModel.cs
+++ PhoenixTelemetryTransfer/ViewModel.cs
@@ -1,22 +1,47 @@
-﻿namespace PhoenixTelemetryTransfer
+﻿// <copyright file="ViewModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace PhoenixTelemetryTransfer
 {
     using System;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
-    using System.Runtime.CompilerServices;
-    using Ookii.Dialogs.Wpf;
-    using System.Windows.Input;
     using System.Configuration;
-    using System.Windows;
-    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
+    using Ookii.Dialogs.Wpf;
 
-    public class ViewModel: INotifyPropertyChanged
+    /// <summary>
+    /// ViewModel for mainWindow.
+    /// </summary>
+    public class ViewModel : INotifyPropertyChanged
     {
         private string path;
         private ObservableCollection<Channel> channels;
-        private int selectedNoChannel;
+        private int selectedNoChannels;
         private int[] noChannels;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModel"/> class.
+        /// </summary>
+        public ViewModel()
+        {
+            this.channels = new ObservableCollection<Channel>();
+            this.noChannels = new int[20] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            this.TryCatch(() => this.LoadConfig());
+            this.BrowseCommand = new RelayCommand(_ => this.TryCatch(() => this.BrowsePath()));
+            this.OnPropertyChanged(nameof(this.NoChannels));
+        }
+
+        /// <summary>
+        /// EventHandler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the number of channels to be used.
+        /// </summary>
         public int[] NoChannels
         {
             get => this.noChannels;
@@ -32,15 +57,128 @@
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public ViewModel()
+        /// <summary>
+        /// Gets or sets Path to file to be subscribed.
+        /// </summary>
+        public string Path
         {
-            this.channels = new ObservableCollection<Channel>();
-            this.noChannels = new int[20] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            this.TryCatch(() => this.LoadConfig());
-            this.BrowseCommand = new RelayCommand(_ => this.TryCatch(() => this.BrowsePath()));
-            this.OnPropertyChanged(nameof(this.NoChannels));
+            get => this.path;
+            set
+            {
+                if (Equals(this.path, value))
+                {
+                    return;
+                }
+
+                this.path = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets Command for browsing path to file that filesubscriber uses.
+        /// </summary>
+        public ICommand BrowseCommand { get; }
+
+        /// <summary>
+        /// Gets command for starting subscribing and writing to PLC.
+        /// </summary>
+        public ICommand StartCommand { get; }
+
+        /// <summary>
+        /// Gets Exception list.
+        /// </summary>
+        public ObservableCollection<Exception> Exceptions { get; } = new ObservableCollection<Exception>();
+
+        /// <summary>
+        /// Gets or sets a list of channels.
+        /// </summary>
+        public ObservableCollection<Channel> Channels
+        {
+            get => this.channels;
+            set
+            {
+                if (ReferenceEquals(value, this.channels))
+                {
+                    return;
+                }
+
+                this.channels = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the selected number of channels to be used.
+        /// </summary>
+        public int SelectedNoChannels
+        {
+            get => this.selectedNoChannels;
+            set
+            {
+                if (Equals(this.selectedNoChannels, value))
+                {
+                    return;
+                }
+
+                this.selectedNoChannels = value;
+                this.OnPropertyChanged();
+
+                // Ugly side effect...
+                this.SelectedNoChannelsChanged();
+            }
+        }
+
+        /// <summary>
+        /// Wrapped TryCatch method and log exceptions to Exceptions list.
+        /// </summary>
+        /// <param name="action">Action to execute.</param>
+        public void TryCatch(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                this.Exceptions.Add(e);
+            }
+        }
+
+        /// <summary>
+        /// Signal to gui that property has changed.
+        /// </summary>
+        /// <param name="propertyName">Optional, nameof property.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void SelectedNoChannelsChanged()
+        {
+            this.UpdateSetting("NoChannels", this.selectedNoChannels.ToString());
+            var noChannels = this.channels.Count;
+
+            if (noChannels < this.selectedNoChannels)
+            {
+                for (int i = noChannels + 1; i <= this.selectedNoChannels; i++)
+                {
+                    var newChannel = new Channel();
+                    newChannel.ChannelName = $"Channel{i}";
+                    this.channels.Add(newChannel);
+                }
+            }
+            else if (this.channels.Count > this.selectedNoChannels)
+            {
+                var channelsToRemove = this.channels.Count - this.selectedNoChannels;
+
+                for (int i = 1; i <= channelsToRemove; i++)
+                {
+                    this.channels.RemoveAt(this.channels.Count - 1);
+                }
+            }
+
+            this.LoadConfig();
         }
 
         private void BrowsePath()
@@ -93,104 +231,6 @@
             configuration.Save();
 
             ConfigurationManager.RefreshSection("appSettings");
-        }
-
-        public string Path
-        {
-            get => this.path;
-            set
-            {
-                if (Equals(this.path, value))
-                {
-                    return;
-                }
-
-                this.path = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public ICommand BrowseCommand { get; }
-
-        public ICommand StartCommand { get; }
-
-        public ObservableCollection<Exception> Exceptions { get; } = new ObservableCollection<Exception>();
-
-        public ObservableCollection<Channel> Channels
-        {
-            get => this.channels;
-            set
-            {
-                if (ReferenceEquals(value, this.channels))
-                {
-                    return;
-                }
-
-                this.channels = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public int SelectedNoChannels
-        {
-            get => this.selectedNoChannel;
-            set
-            {
-                if (Equals(this.selectedNoChannel, value))
-                {
-                    return;
-                }
-
-                this.selectedNoChannel = value;
-                this.OnPropertyChanged();
-
-                // Ugly side effect...
-                this.SelectedNoChannelsChanged();
-            }
-        }
-
-        private void SelectedNoChannelsChanged()
-        {
-            this.UpdateSetting("NoChannels", this.selectedNoChannel.ToString());
-            var noChannels = this.channels.Count;
-
-            if (noChannels < this.selectedNoChannel)
-            {
-                for (int i = noChannels + 1; i <= this.selectedNoChannel; i++)
-                {
-                    var newChannel = new Channel();
-                    newChannel.ChannelName = $"Channel{i}";
-                    this.channels.Add(newChannel);
-                }
-            }
-            else if (this.channels.Count > this.selectedNoChannel)
-            {
-                var channelsToRemove = this.channels.Count - this.selectedNoChannel;
-
-                for (int i = 1; i <= channelsToRemove; i++)
-                {
-                    this.channels.RemoveAt(this.channels.Count-1);
-                }
-            }
-
-            this.LoadConfig();
-        }
-
-        public void TryCatch(Action action)
-        {
-            try
-            {
-                action();
-            }
-            catch (Exception e)
-            {
-                this.Exceptions.Add(e);
-            }
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
