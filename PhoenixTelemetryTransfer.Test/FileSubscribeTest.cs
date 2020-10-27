7e879a77ad74ba177c476365535c51daa6939d2d
MartinNorberg <martin.norberg@gautomation.se>
2020-10-27 08:49:44 +0100

    removed delta from tests

--- PhoenixTelemetryTransfer.Test/FileSubscribeTest.cs
+++ PhoenixTelemetryTransfer.Test/FileSubscribeTest.cs
@@ -34,7 +34,7 @@
 
             if (fileSubscriber.TryGetValues("someformat?;99,99", out var result))
             {
-                Assert.AreEqual(99.99f, double.Parse(result[0]), 0.1f);
+                Assert.AreEqual(99.99, double.Parse(result[0]));
             }
 
         }
@@ -46,7 +46,7 @@
 
             if (fileSubscriber.TryGetValues("00:01:12;    20,14", out var result))
             {
-                Assert.AreEqual(20.14f, double.Parse(result[0]), 0.1f);
+                Assert.AreEqual(20.14, double.Parse(result[0]));
             }
         }
 
@@ -57,8 +57,8 @@
 
             if (fileSubscriber.TryGetValues("00:01:13;    20,14;   20,19", out var result))
             {
-                Assert.AreEqual(20.14f, double.Parse(result[0]), 0.1f);
-                Assert.AreEqual(20.19f, double.Parse(result[1]), 0.1f);
+                Assert.AreEqual(20.14, double.Parse(result[0]));
+                Assert.AreEqual(20.19, double.Parse(result[1]));
             }
 
         }
@@ -70,7 +70,7 @@
 
             if (fileSubscriber.TryGetValues("00:01:13; 20,19", out var result))
             {
-                Assert.AreEqual("00:01:13", fileSubscriber.TimeStamp); 
+                Assert.AreEqual("00:01:13", fileSubscriber.TimeStamp);
             }
 
         }
@@ -84,26 +84,26 @@
                 "00:01:13;  20,19;   21,534;     22,54;  19,51;  24,89;  19,0;   24,65;   20;    21,15;  24,501;    11,96;" +
                 "   23,15;     22,11;  23,65;  15,95;  21,546;     17,62;  21,77;  19,01;  20,02", out var result))
             {
-                Assert.AreEqual(20.19f, double.Parse(result[0]), 0.1f);
-                Assert.AreEqual(21.534f, double.Parse(result[1]), 0.1f);
-                Assert.AreEqual(22.54f, double.Parse(result[2]), 0.1f);
-                Assert.AreEqual(19.51f, double.Parse(result[3]), 0.1f);
-                Assert.AreEqual(24.89f, double.Parse(result[4]), 0.1f);
-                Assert.AreEqual(19.0f, double.Parse(result[5]), 0.1f);
-                Assert.AreEqual(24.65f, double.Parse(result[6]), 0.1f);
-                Assert.AreEqual(20f, double.Parse(result[7]), 0.1f);
-                Assert.AreEqual(21.15f, double.Parse(result[8]), 0.1f);
-                Assert.AreEqual(24.501f, double.Parse(result[9]), 0.1f);
-                Assert.AreEqual(11.96f, double.Parse(result[10]), 0.1f);
-                Assert.AreEqual(23.15f, double.Parse(result[11]), 0.1f);
-                Assert.AreEqual(22.11f, double.Parse(result[12]), 0.1f);
-                Assert.AreEqual(23.65f, double.Parse(result[13]), 0.1f);
-                Assert.AreEqual(15.95f, double.Parse(result[14]), 0.1f);
-                Assert.AreEqual(21.546f, double.Parse(result[15]), 0.1f);
-                Assert.AreEqual(17.62f, double.Parse(result[16]), 0.1f);
-                Assert.AreEqual(21.771f, double.Parse(result[17]), 0.1f);
-                Assert.AreEqual(19.01f, double.Parse(result[18]), 0.1f);
-                Assert.AreEqual(20.02f, double.Parse(result[19]), 0.1f);
+                Assert.AreEqual(20.19, double.Parse(result[0]));
+                Assert.AreEqual(21.534, double.Parse(result[1]));
+                Assert.AreEqual(22.54, double.Parse(result[2]));
+                Assert.AreEqual(19.51, double.Parse(result[3]));
+                Assert.AreEqual(24.89, double.Parse(result[4]));
+                Assert.AreEqual(19.0, double.Parse(result[5]));
+                Assert.AreEqual(24.65, double.Parse(result[6]));
+                Assert.AreEqual(20, double.Parse(result[7]));
+                Assert.AreEqual(21.15, double.Parse(result[8]));
+                Assert.AreEqual(24.501, double.Parse(result[9]));
+                Assert.AreEqual(11.96, double.Parse(result[10]));
+                Assert.AreEqual(23.15, double.Parse(result[11]));
+                Assert.AreEqual(22.11, double.Parse(result[12]));
+                Assert.AreEqual(23.65, double.Parse(result[13]));
+                Assert.AreEqual(15.95, double.Parse(result[14]));
+                Assert.AreEqual(21.546, double.Parse(result[15]));
+                Assert.AreEqual(17.62, double.Parse(result[16]));
+                Assert.AreEqual(21.77, double.Parse(result[17]));
+                Assert.AreEqual(19.01, double.Parse(result[18]));
+                Assert.AreEqual(20.02, double.Parse(result[19]));
             }
         }
     }
